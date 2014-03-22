e1989eec2a49f70c791cefbad5ce7b08ddff28b7
ryAn <ryAn@fq2-wireless-pittnet-47-33.wireless.pitt.edu>
2014-03-22 12:54:37 -0400

    Changed the output todisplay the message received by the server

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
+ 	
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message("Hello! You're message was: ", request.value.get('Body', None))
     return str(resp)
  
 if __name__ == "__main__":
