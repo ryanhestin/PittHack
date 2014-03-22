77f8bfbf516c79f4c5f7a6c0befb2f3a0d27a4a3
ryAn <ryAn@fq2-wireless-pittnet-47-33.wireless.pitt.edu>
2014-03-22 11:57:44 -0400

    Changed the run app to be SMS instead of MMS oriented

--- run.py
+++ run.py
@@ -8,8 +8,7 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
  
     resp = twilio.twiml.Response()
-    with resp.message("Hello, Mobile Monkey") as m:
-        m.media("https://demo.twilio.com/owl.png")
+    resp.message("Hello, Mobile Monkey")
     return str(resp)
  
 if __name__ == "__main__":
