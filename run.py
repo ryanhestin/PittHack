0377c4f0e6aa87e5ff7353fe033d1e8ae925b05f
ryAn <ryAn@fq2-wireless-pittnet-47-33.wireless.pitt.edu>
2014-03-22 12:56:22 -0400

    fixed error in resp.message

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
  	
     resp = twilio.twiml.Response()
-    resp.message("Hello! You're message was: ", request.value.get('Body', None))
+    resp.message("Hello! You're message was: " + request.value.get('Body', None))
     return str(resp)
  
 if __name__ == "__main__":
