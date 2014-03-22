b9fa2a7276c871f6a84dc254b2c9b792db2ab2a1
ryAn <ryAn@fq2-wireless-pittnet-47-33.wireless.pitt.edu>
2014-03-22 13:19:44 -0400

    Nope...

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
  	
     resp = twilio.twiml.Response()
-    resp.message(request.REQUEST.get('Body', None))
+    resp.message("Woof.")
     #print(request.value.get('Body', None))
 
 
