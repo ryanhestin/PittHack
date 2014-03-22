91a96a1a2985b94f9e6d16a8d2d9ec0c1654986a
ryAn <ryAn@fq2-wireless-pittnet-47-33.wireless.pitt.edu>
2014-03-22 13:52:54 -0400

    fail

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
  	
     resp = twilio.twiml.Response()
-    output = request.Message.Body
-    resp.message("Woof. " + output)
+    #output = request.Message.Body
+    resp.message("Woof. " )#+ output)
     #print(request.value.get('Body', None))
 
 
