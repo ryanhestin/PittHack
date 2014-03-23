6b3de726d222175c2716653906e091f2af88ddb6
ryAn <ryAn@fq2-wireless-pittnet-46-128.wireless.pitt.edu>
2014-03-23 10:46:30 -0400

    Cleaned up the !commands string

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def respond_to_query():
     print(body)
 
     if '!commands' in body:
-        resp.message("Wikter Commands:\n-Type any series of words to search wikipedia for info\n-Type any search then \"-<0-20>\" to page through resulting sentences\n-Type \"@<twitter_handle>\" to get the latest tweet from any twitter handle\n-Type \"@<twitter_handle> -<2-20>\" to earlier tweets\n-Type a math or general knowledge in the form \"<query> -w\" to try WolframAlpha")
+        resp.message("Commands:\n-Type any series of words to search wikipedia for info\n-Type any search then \"-<0-20>\" to page through resulting sentences\n-Type \"@<twitter_handle>\" to get the latest tweet from any twitter handle\n-Type \"@<twitter_handle> -<2-20>\" to earlier tweets\n-Type a math or general knowledge in the form \"<query> -w\" to try WolframAlpha")
         return str(resp)
 
     if body[0] == "@":
