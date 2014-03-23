2b520b28facc8e0bc8a7ed254eff4b92e43e1d08
ryAn <ryAn@fq2-wireless-pittnet-47-215.wireless.pitt.edu>
2014-03-22 20:55:46 -0400

    Small changes to the help output

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def respond_to_query():
     print(body)
 
     if '!commands' in body:
-        resp.message("Wikter Commands:\n-Type any series of words to search wikipedia for info\n-Type any search then \"-<0-20>\" to get more info on the topic\n-Type \"@<twitter_handle>\" to get the latest tweet from any twitter handle\n-Type \"@<twitter_handle> -<1-9>\" to get different tweets")
+        resp.message("Wikter Commands:\n-Type any series of words to search wikipedia for info\n-Type any search then \"-<0-20>\" to page through resulting sentences\n-Type \"@<twitter_handle>\" to get the latest tweet from any twitter handle\n-Type \"@<twitter_handle> -<2-20>\" to earlier tweets")
         return str(resp)
 
     if body[0] == "@":
